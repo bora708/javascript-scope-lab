205916e996c7243c9b232c40a8dd7a6e9aabe8cc
bora <borahassan74@gmail.com>
2026-04-16 09:39:52 -0700

    Complete JavaScript scope lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// burgers list
+const burgers = ["Hamburger", "Cheeseburger"]
+
+// drink
+let featuredDrink = "Strawberry Milkshake"
+
+
+// add burger function
+function addBurger() {
+  const newBurger = "Flatburger"
+  burgers.push(newBurger)
+}
+
+
+// this always runs
+if(true) {
+  const anotherNewBurger = "Maple Bacon Burger"
+  burgers.push(anotherNewBurger)
+}
+
+
+// change drink
+function changeFeaturedDrink() {
+  featuredDrink = "The JavaShake"
+}
